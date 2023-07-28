758772c99fd3a479782e65a0f8e5b752b8d8bca3
sangkhim <sangkhim.allweb@gmail.com>
2023-07-28 15:56:07 +0700

    update swagger

--- src/main/java/com/sangkhim/spring_boot3_ws/config/SwaggerConfig.java
+++ src/main/java/com/sangkhim/spring_boot3_ws/config/SwaggerConfig.java
@@ -1,9 +1,14 @@
 package com.sangkhim.spring_boot3_ws.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,9 +19,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SwaggerConfig implements WebMvcConfigurer {
 
   private static final String REDIRECT_URL = "/swagger-ui.html";
+  private static final String OAUTH_SCHEME_NAME = "client-sangkhim";
 
   @Value("${spring.mvc.servlet.path}")
   private String baseUrl;
+
+  @Value("${keycloak.auth-server-url}")
+  String authServerUrl;
+
+  @Value("${keycloak.realm}")
+  String realm;
 
   @Override
   public void addViewControllers(final ViewControllerRegistry registry) {
@@ -37,6 +49,15 @@ public class SwaggerConfig implements WebMvcConfigurer {
         .externalDocs(
             new ExternalDocumentation()
                 .description("Documentation")
-                .url("https://t.me/boreytechnology"));
+                .url("https://t.me/boreytechnology"))
+        .components(
+            new Components()
+                .addSecuritySchemes(
+                    OAUTH_SCHEME_NAME,
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.OAUTH2)
+                        .in(SecurityScheme.In.HEADER)
+                        .flows(new OAuthFlows().password(new OAuthFlow().tokenUrl(authServerUrl)))))
+        .addSecurityItem(new SecurityRequirement().addList(OAUTH_SCHEME_NAME));
   }
 }
